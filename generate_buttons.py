38a6e26fed218e3fb8cff793f36bfbf7778f1d50
John Gage <john.gage@gmail.com>
2023-08-22 10:39:54 -0700

    08-24-0 again

--- generate_buttons.py
+++ generate_buttons.py
@@ -5,5 +5,5 @@ with open("_data/pdf_urls.yml", "r") as yaml_file:
 
 with open("output.html", "w") as html_file:
     for key, value in data.items():
-        html_button = f'<a class="button" href="{value}">Visit PDF for {key}</a>'
+        html_button = f'<a class="button" href="{value}">Visit PDF for page {key}</a>'
         html_file.write(html_button + "\n")
